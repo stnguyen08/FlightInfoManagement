3e9ce86148665d35bcac1257c5ea2255c51f64fe
Son Nguyen <ngtson@gmail.com>
2018-09-21 17:08:30 -0500

    init

---
+++ src/main/java/cs544/flight/FlightManagementApplication.java
@@ -0,0 +1,12 @@
+package cs544.flight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightManagementApplication.class, args);
+    }
+}
