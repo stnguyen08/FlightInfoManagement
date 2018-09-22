44d222480cf46685f4ccec203b3774aaa25602fc
Phuong68 <phuong.lehoang@gmail.com>
2018-09-22 11:46:08 -0500

    importdata

--- src/main/java/cs544/flight/controller/AirlineController.java
+++ src/main/java/cs544/flight/controller/AirlineController.java
@@ -1,4 +1,54 @@
 package cs544.flight.controller;
 
+import cs544.flight.domain.Airline;
+import cs544.flight.persistence.IAirlinePersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
 public class AirlineController implements IAirlineController {
+
+    @Autowired
+    private IAirlinePersistence airlinePersistence;
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/airlines", method = RequestMethod.GET)
+    public List<Airline> getAll(Model model) {
+        model.addAttribute("airlines", airlinePersistence.getAll());
+        return "airlines";
+    }
+
+    @Override
+    public String addBook(Airline book) {
+        return null;
+    }
+
+    @Override
+    public String add(Airline book, BindingResult result, RedirectAttributes re) {
+        return null;
+    }
+
+    @Override
+    public String get(int id, Model model) {
+        return null;
+    }
+
+    @Override
+    public String update(Airline book, BindingResult result, int id) {
+        return null;
+    }
+
+    @Override
+    public String delete(int bookId) {
+        return null;
+    }
 }
--- src/main/java/cs544/flight/controller/IAirlineController.java
+++ src/main/java/cs544/flight/controller/IAirlineController.java
@@ -1,4 +1,29 @@
 package cs544.flight.controller;
 
+import cs544.flight.domain.Airline;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.List;
+
 public interface IAirlineController {
+
+
+    public abstract List<Airline> getAll();
+
+    public abstract String addBook(Airline book);
+
+    public  abstract String add(Airline book, BindingResult result, RedirectAttributes re) ;
+
+    public abstract String get(int id, Model model) ;
+
+    public abstract  String update(Airline book, BindingResult result, int id) ;
+
+    public abstract String delete(int bookId) ;
+
+
 }
